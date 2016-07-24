ceaa46fa4559c65010cb7742782eb083a99134ca
luis <luiscarlin@gmail.com>
2016-07-24 03:36:35 -0400

    Added tests

--- Makefile
+++ Makefile
@@ -5,3 +5,6 @@ build:
 	gcc -std=c11 -Wall sudoku-solver.c -o sudoku-solver
 clean:
 	rm -f sudoku-solver
+
+test: build
+	./run-tests.sh tests.txt 
---
+++ run-tests.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+index=0
+failed=0
+passed=0
+
+while read -r line || [[ -n "$line" ]]
+do
+    if ! [[ -z "$line" ]] 
+    then
+        array[$index]="$line"
+        index=$((index+1))
+    fi
+done < "$1"
+
+index=0
+
+
+echo
+echo Running Tests
+echo =============
+while [ $index -lt ${#array[@]} ]
+do
+    echo -n ${array[$index]}
+    index=$((index+1))
+    
+    result=$(./sudoku-solver -i ${array[$index]})
+    index=$((index+1))
+
+    if [[ $result == ${array[$index]} ]]
+    then
+	echo " ==> Passed"
+        passed=$((passed+1))
+    else
+        echo " ==> Failed"
+        failed=$((failed+1))
+    fi
+    index=$((index+1)) 
+done
+echo
+echo Summary
+echo =======
+echo "    Passed = $passed"
+echo "    Failed = $failed"
+echo
+
